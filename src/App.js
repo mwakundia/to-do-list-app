891f5f886a994620cab344c96ed252d04e7106ce
mwakundia <brianmwakundia@gmail.com>
2024-02-29 19:17:20 +0300

    todo app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+// import TodoForm from './componentss/TodoForm';
+import TodoList from './componentss/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+        {/* <h1>Todo app</h1> */}
+        <TodoList/>
+    </div>
+  );
+}
+
+export default App;
